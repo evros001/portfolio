c66de3fa23a3dfe7c0869d0c242a454724c41548
evros001 <evanrosse@gmail.com>
2015-06-27 15:01:58 -0400

    set up admin and Post and Category models

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source 'https://rubygems.org'
 
-
+#admin
+gem 'rails_admin' 
+# authentication
+gem 'devise'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.1'
 # Use postgresql as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  
+  resources :posts
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users
+  root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
